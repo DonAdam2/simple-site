445d808054e985678599dc644138a0f1aabba950
Adam Morsi <adam.morsi@yahoo.com>
2021-02-05 15:39:19 +0200

    update footer component

--- src/components/footer/AppFooter.jsx
+++ src/components/footer/AppFooter.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 //styles
 import styles from './AppFooter.module.scss';
 
-const AppFooter = () => <footer className={styles.footer}>App Footer.</footer>;
+const AppFooter = () => <footer className={styles.footer}>Copyright 2021 Adam Morsi.</footer>;
 
 export default AppFooter;
