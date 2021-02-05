76ba46053092ee45fe8c4191ecae0487f7125edf
Adam Morsi <adam.morsi@yahoo.com>
2021-02-05 16:13:20 +0200

    remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,10 @@ import React from 'react';
 import styles from '../styles/pages/Home.module.scss';
 //containers
 import PageWrapper from '../components/shared/pageWrapper/PageWrapper';
-//components
-import TestComponent from '../components/testComponent/TestComponent';
 
 const Home = () => (
 	<PageWrapper title="Home">
 		<div>Home page</div>
-		<TestComponent />
 	</PageWrapper>
 );
 
